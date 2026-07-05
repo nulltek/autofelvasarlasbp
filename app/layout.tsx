2f44435c6f1a8cfb458fd56f150291f2acf24164
nulltek <nulltek0@gmail.com>
2026-07-05 19:15:50 +0200

    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { getSettings } from "@/lib/db";
@@ -45,6 +46,15 @@ export default async function RootLayout({
   return (
     <html lang="hu">
       <body>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-GQY82J9JFN" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-GQY82J9JFN');
+          `}
+        </Script>
         <Header settings={settings} />
         <main>{children}</main>
         <Footer settings={settings} />
